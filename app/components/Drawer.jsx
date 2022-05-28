f5449e6af3e7a2f04764a278c846f7ef38eb1f80
Tyler Duke <tduke@Tylers-MacBook-Air.local>
2022-05-28 08:35:14 -0400

    Componentized Drawer

---
+++ app/components/Drawer.jsx
@@ -0,0 +1,10 @@
+
+export default function Drawer() {
+    return (
+        <div className="flex justify-center items-center w-[97.5%] h-32 position absolute bottom-2 left-2 bg-black">
+            <div className="flex justify-center items-center w-1/2 h-full position absolute bottom-4 bg-blue-700 border border-4 border-green-400 rounded-lg">
+                <h1 className="text-7xl">Prompt</h1>
+            </div>
+        </div>
+    )
+}
